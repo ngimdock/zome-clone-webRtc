7e2087b00c9d81657ae7b856f2ad3e7d57cdd8e7
ngimdock <ngimdockzemfack@gmail.com>
2023-02-05 16:08:03 +0100

    Add video stream

--- public/script.js
+++ public/script.js
@@ -1,13 +1,43 @@
 const socket = io();
 
+const peer = new Peer(undefined, {
+  host: "/",
+  port: "5001",
+});
+
 const USER_ID = 10;
 
-socket.emit("join-room", ROOM_ID, USER_ID);
+peer.on("open", (userId) => {
+  socket.emit("join-room", ROOM_ID, userId);
+});
 
 socket.on("user-connected", (userId) => {
-  console.log(`The user #${userId} connected.`);
+  console.log(`The user connected: ${userId}`);
 });
 
 socket.on("user-disconnected", (userId) => {
-  console.log(`The user #${userId} disconnected `);
+  console.log(`The user disconnected: ${userId}`);
 });
+
+const videoGrid = document.getElementsByClassName("video-grid");
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+  });
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+
+  videoGrid[0].append(video);
+}
